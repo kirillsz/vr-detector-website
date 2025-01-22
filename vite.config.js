87632c88b43ca6f69b8c98a7c758f51892ab838b
kirillsz <shakhov2013@list.ru>
2025-01-22 20:03:51 +0300

    update: Updates vite config for gh-pages

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import eslint from "vite-plugin-eslint";
 
 export default defineConfig({
+  base: "/vr-detector-website/",
   publicDir: "public",
   root: "./",
   build: {
